21889faaaab90046bab5c5607898728e0b5de355
Sarmistha Nayak <er.sarmi2310@gmail.com>
2024-06-21 22:46:56 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <HeaderVue/>
+  <ContentHeader/>
+  <MainContent/>
+</template>
+
+<script>
+import HeaderVue from './components/HeaderVue.vue'
+import ContentHeader from './components/ContentHeader.vue'
+import MainContent from './components/MainContent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderVue,
+    ContentHeader,
+    MainContent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Roboto', sans-serif;
+  text-align: center;
+}
+
+body {
+  margin-top: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,175 @@
+<template>
+    <section class="content">
+        <div class="title">
+            <h2>Want to meet random people online?</h2>
+        </div>
+        <div class="subtitle">
+            <h3>We are here with our top video chat sites in one place.</h3>
+        </div>
+        <div class="desp">
+            <p>Video Chatting Co. provides private 1on1 video chat with girls and random strangers online. Bored alone? Or, want someone to chill with? Worry not, because, we have the best random video chatting with girls for free sites. Furthermore, you can enjoy a roulette chat with girls instantly. Video Chatting Co. ensures that all the users in our platform are totally secured and safe from outsiders, so we have collaborated with our partners live to provide the best service by the experienced players in the market. Video Chatting Co. offers a variety of chat roulette services, specially built for having a luxurious video chat with girls at a freemium price.</p>
+            <p>To start a chat for free, you are not required to register or give credit card info, simply click and start video chatting with girls today.</p>
+        </div>
+
+    </section>
+
+    <section class="sub-content">
+        <div class="title">
+            <h2>1on1 private chat with girls</h2>
+        </div>
+        <div class="desp">
+            <p>Are you bored now? Looking for a free video chat with girls online live? We have a bunch of adult video chat roulette site collections where you can enjoy a free video chat with girls for free. Right now, we have collaborated with many live cam chat sites, mostly focused on 1on1 private video chats. Furthermore, Video Chatting Co. is an all-rounder video calling and live dating site where you have the options to select gender, filter location, apply a face mask or even more with our premium VIP dating plans. Our Video Chatting Co. chat app will be launching soon, with more awesome features. Wondering why we are, still now? We are the best adult video chatting provider and here you can enjoy a live video chat with stranger girls online. Now, we are one of the best video chat providers with a full screen mode, where you can enjoy the benefit of having a large chat size for clear visibility. With a wider resolution, you can enjoy adult cam chat with girls more swiftly.</p>
+        </div>
+    </section>
+
+    <section class="sub-content">
+        <div class="title">
+            <h2>Why to choose us?</h2>
+        </div>
+        <div class="desp">
+            <p>We are very happy to provide you our amazing 1on1 private live video chat service with stranger girls online. There are many adult video chats in the market globally, but we are the best Omegle alternative and Onlyfans alternative because we can provide a full screen video chat service to you.</p>
+            <p style="color: #4695F8; font-style: italic; font-weight: bold;">Watching and enjoying a roulette chat service in a full screen mode will give you a pleasure to a next level. Try our chat sites today.</p>
+        </div>
+    </section>
+
+    <section class="sub-content">
+        <div class="title">
+            <h2>Best Omegle Alternative? Really?</h2>
+        </div>
+        <div class="desp">
+          <p>Omegle video chat site is the oldest player in the market with everything added to their site, making the site super slow on the client’s browser. Also, Omegle have no gender filter option till today, there is a ton of ads and captcha code that will ruin the user experience and will serve you a bulky interface. All these barriers are easily removed by Video Chatting Co., we have partnered with instant chat rooms and top chat sites in the
+           market to provide the best 1on1 private video call with online girls. We are the best Omegle alternative as of now and will provide the best chat experience, as we can. On the Omegle alternative, you will get random video chat with strangers, instantly, with a gender filter and location filter option.</p>
+        </div>
+    </section>
+
+    <section class="sub-content">
+        <div class="title">
+            <h2>Like, OnlyFans alternative for free?</h2>
+        </div>
+        <div class="desp">
+          <p>Onlyfans is a service where boys have to register and create an Onlyfans account and give their credit card detail and other valuable information, it is time-consuming. Just to enjoy an adult video chat with girls, you have to provide all the information and subscribe to their plans, there is no free option to enjoy with girls. Video Chatting Co. brought the best solution to this, we are classified as the best Onlyfans alternative, here
+            you can enjoy live 1on1 video chat, with strangers, even for free. Furthermore, Video Chatting Co. provides a random adult video chat with girls instantly with a single click. So, what are you waiting for? Just click on Onlyfans alternative and get a free adult video chat with girls live online instantly.</p>
+          </div>
+    </section>
+
+    <section class="sub-content">
+        <div class="title">
+            <h2>Using Coomeet for chatting with girls only</h2>
+        </div>
+        <div class="desp">
+          <p>Coomeet is an online dating service where you can enjoy a 1on1 private video chat directly with thousands of verified cam girls on the web. Coomeet by Video Chatting Co. is a specially launched video dating web service. With us, you get the best offer of up to 50% discount on our premium plans.</p>
+          <p>If you are alone and want, to meet someone online, to satisfy yourself, you can try Coomeet video chat today. You can start a private chat with girls for free with some limitations, like capped minutes on roulette chat, limited filter option, and face detection enabled. However, if are on the premium plans that start from only 1.99$, you can enjoy it fully. You can talk with a particular girl until your minutes-credit is exhausted.</p>
+          <p>So, Coomeet is one of the top chat sites where you can enjoy cam to cam chat with girls online, even for free. Try it, you will love it for sure. So, what are you waiting for? Feeling lonely? Meet random cam girls online today.</p>
+          </div>
+    </section>
+
+    <section class="content">
+        <div class="title">
+            <h2>Features of Video Chatting Co.</h2>
+        </div>
+        <div class="card-container">
+            <div class="card">
+                <p>logo</p>
+                <h3>Effortless Access</h3>
+                <p>Begin video chatting with few simple clicks, and discover a multitude of authenticated live cam girls with our online video chat platform.</p>
+            </div>
+
+            <div class="card">
+                <p>logo</p>
+                <h3>Complete Privacy</h3>
+                <p>VideoChatting.co ensures absolute anonymity during both video chats, thanks to robust backend protocols, that keeps you in safe.</p>
+            </div>
+        </div>
+        <div class="card-container">
+            <div class="card">
+                <p>logo</p>
+                <h3>Instant Matchup</h3>
+                <p>Get ideal partner instantly, with rapid matchmaking algorithm, connecting you with a soulmate every time you next someone.</p>
+            </div>
+
+            <div class="card">
+                <p>logo</p>
+                <h3>Verified CamGirls</h3>
+                <p>Discover a wide selection of verified cam girls, where you can enjoy live video chat for free, making your experience truly enjoyable.</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+
+<script>
+export default {
+    name: 'MainContent'
+}
+</script>
+
+<style scoped>
+ .content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+}
+
+.sub-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+}
+
+.sub-content .title h2{
+    font-size: 40px;
+}
+
+.title h2 {
+  color: #4695F8;
+  font-size: 55px;
+  font-weight: 550;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: #000;
+  text-shadow: 1px 1px 0px #000000;
+  margin: 0;
+}
+
+.subtitle h3 {
+  color: black;
+  font-size: 24px;
+  margin-top: 20px;
+  text-decoration: underline;
+}
+
+.desp {
+  max-width: 999px; /* Set a max-width for better readability */
+  margin: 0; /* Add some margin for spacing */
+}
+
+.desp p {
+  color: black;
+  font-size: 16px;
+  text-align: justify;
+  margin: 10px 0; /* Adjust margins for spacing between paragraphs */
+  line-height: 1.5;
+}
+
+.card-container{
+    display: flex;
+}
+
+.card {
+    align-items: center;
+    justify-content: center;
+    border-radius: 12px;
+    border: solid 3px #000;
+    padding: 10px 10px 0px 10px;
+    text-align: center;
+    margin: 10px;
+    transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out; 
+}
+
+</style>
